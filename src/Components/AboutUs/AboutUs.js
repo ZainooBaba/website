4d9b58d64c449d736377da34a1ede70b78f2fff5
Anatolii Lehkyi <lanot.biz@gmail.com>
2023-07-27 22:49:10 +0300

    CF-37 About Us section - draft - need to adjust widths of tiles

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './AboutUs.css';
 import bkgrd from "../../assets/about-us/bkgrd.svg";
+import line from "../../assets/about-us/line.svg";
 
-export function AboutUs () {
+function AboutUsTop() {
   return (
     <div className="flex-container">
       <div className="about-us-col1">
-        <img src={bkgrd} alt={"Abous Us"} className="AboutUsImg" border={0}/>
+        <img src={bkgrd} alt={"About Us"} className="AboutUsImg" border={0}/>
       </div>
 
       <div className="about-us-col2 flex-col-jv">
@@ -25,5 +26,50 @@ export function AboutUs () {
       </div>
     </div>
   );
-};
+}
 
+function AboutUsBottom() {
+  return (
+    <div className="flex-container">
+
+      <div className="about-us-tile">
+        <div>
+          <h6 className="text">Start</h6>
+          <b1 className="text">Our journey with SAP started with eProcurement in the late 1990’s</b1>
+        </div>
+      </div>
+
+      <div className="about-us-tile">
+        <div>
+          <h6 className="text">Consulting Journey</h6>
+          <b1 className="text">In the ensuing 25 years, we implemented Supply Chain solutions at over 150+ Fortune 1000 companies, and created an award winning Source-2-Pay consulting company, Nitor Partners.</b1>
+        </div>
+      </div>
+
+      <div className="about-us-tile">
+        <div>
+          <h6 className="text">SaaS Success</h6>
+          <b1 className="text">Switching from consulting to SaaS: we launched BuyerQuest - an eProcurement SaaS solution. Designed for with a B2C focus and fully integrated with SAP-Ariba, it is the #1 mid-market Procure-2-Pay solution, with global deployments at marquee clients, including McDonalds, BASF, KimberlyClark, Saudi Aramco, Disney, and Chick-fil-A</b1>
+        </div>
+      </div>
+
+      <div className="about-us-tile">
+        <div>
+          <h6 className="text">Today</h6>
+          <b1 className="text">Combining our consulting DNA with SaaS expertise, Code Fluent emerges to accelerate the effectiveness, connectedness and visibility of supply chains of SAP’s customer base.</b1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export function AboutUs () {
+  return (
+    <div>
+      <AboutUsTop />
+      <div className="about-us-line" style={{backgroundImage: `url(${line})`}}/>
+      <AboutUsBottom />
+    </div>
+  );
+};
